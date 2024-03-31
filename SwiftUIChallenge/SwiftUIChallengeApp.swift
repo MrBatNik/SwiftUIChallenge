7367cdbfe91cf96484f043edf77e1da92422d972
Nikita <nicandreo@gmail.com>
2024-03-31 11:52:43 +0400

    Initial Commit

---
+++ SwiftUIChallenge/SwiftUIChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIChallengeApp.swift
+//  SwiftUIChallenge
+//
+//  Created by Nikita Batrakov on 31.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
